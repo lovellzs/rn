9bfa948df4596f5e05539b96bac3eca817f63d22
shizhe <apple@shizhe.local>
2017-10-24 18:12:22 +0800

    State 状态

--- AwesomeProject/中文网学习/State（状态）.js
+++ AwesomeProject/中文网学习/State（状态）.js
@@ -4,31 +4,26 @@ import { AppRegistry, Text, View } from 'react-native';
 class Blink extends Component {
     constructor(props) {
         super(props);
-        this.state = { showText: true ,num:1};
+        this.state = { showText: true };
+
         // 每1000毫秒对showText状态做一次取反操作
         setInterval(() => {
             this.setState(previousState => {
-                return { showText: !previousState.showText ,num:++previousState.num};
+                return { showText: !previousState.showText };
             });
         }, 1000);
     }
 
-    //this.setState 可以传入一个函数，函数参数可以传入前一个状态，返回后一个状态
-
     render() {
         // 根据当前showText的值决定是否显示text内容
         let display = this.state.showText ? this.props.text : ' ';
-        let num = this.state.num ;
         return (
-            <View>
-                <Text>{display}</Text>
-                <Text>{num}</Text>
-            </View>
+            <Text>{display}</Text>
         );
     }
 }
 
-class AwesomeProject extends Component {
+class BlinkApp extends Component {
     render() {
         return (
             <View>
@@ -41,4 +36,4 @@ class AwesomeProject extends Component {
     }
 }
 
-AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
+AppRegistry.registerComponent('BlinkApp', () => BlinkApp);
